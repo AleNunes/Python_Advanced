04f9a52411b8512152c1e00eae6e9c272d8f203a
Alexander Nunes <alexandercn@hotmail.com>
2016-08-22 19:51:42 -0300

    Aula 05

---
+++ HandsOn/Aula06/Jenkins.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import jenkins
+
+class Jenkins:
+    def __init__(self):
+        try:
+            self.server = jenkins.Jenkins("http://192.168.0.4:8080")
+            print self.server.get_version()
+        except Exception as e:
+            print "Nao foi possivel conectar ao Jenkins: ", e    
+        
+    def create_job(self, name):
+        try:
+            self.server.create_job(name, jenkins.EMPTY_CONFIG_XML)
+            print "Job criada com sucesso!"
+        except Exception as e:
+            print "Falhou ao criar job: ", e     
+            
+        
+if __name__=="__main__":
+    j = Jenkins()
+    j.create_job("Job Python")
+        
