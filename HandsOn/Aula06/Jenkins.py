51892c352885ccbcb496c64938ff70ba50c6a22a
Alexander Nunes <alexandercn@hotmail.com>
2016-08-22 20:48:29 -0300

    Aula 05

--- HandsOn/Aula06/Jenkins.py
+++ HandsOn/Aula06/Jenkins.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import jenkins
+from lxml import etree
 
 class Jenkins:
     def __init__(self):
@@ -12,16 +13,31 @@ class Jenkins:
         
     def create_job(self, name):
         try:
-            print  jenkins.EMPTY_CONFIG_XML
+            with open("templates/job.xml","r") as f:
+                xml = f.read().replace("REPO_TESTE","git@git.com.br")
+            
+            xml = self.generate_steps(xml)
+                
+            #print  jenkins.EMPTY_CONFIG_XML
             self.server.create_job(name, jenkins.EMPTY_CONFIG_XML)
             print "Job criada com sucesso!"
         except Exception as e:
             print "Falhou ao criar job: ", e     
             
-        
+
+
+    def generate_steps(self, xml):
+        root = etree.XML(xml)
+        for b in root.findall("builders"):
+            builder = b
+        for c in builder.iterchildren():
+            print c.tag, c.text
+            for j in c.iterchildren():
+                print j.tag
+            
 if __name__=="__main__":
     j = Jenkins()
-    j.create_job("Job Python")
+    j.create_job("Job Python 2")
 
 
 
